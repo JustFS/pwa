68b74dd4f21c43625d9d64891ff5429b44545021
Julien Az <julien.azbrg@gmail.com>
2020-12-24 10:22:25 +0100

    added offline

--- sw.js
+++ sw.js
@@ -1,9 +1,41 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
+
 // install event
 self.addEventListener("install", (evt) => {
-  console.log("service worker installed");
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+      // install events is not finished until this promess is resolved
+    })
+  );
 });
 
 // activate event
 self.addEventListener("activate", (evt) => {
-  console.log("service worker activated");
+  //console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener("fetch", (evt) => {
+  // test event when offline
+  // console.log("fetch event", evt);
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request);
+    })
+  );
 });
