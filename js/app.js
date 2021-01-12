b85c82de06082ad122ba01332dea7edc6a59e166
Julien Az <julien.azbrg@gmail.com>
2021-01-12 14:53:07 +0100

    démarrage

--- js/app.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/serviceWorker.js")
-    .then((reg) => console.log("service worker registered"))
-    .catch((err) => console.log("service worker not registered", err));
-}
