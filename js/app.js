2c35ef4591c0c471ab36426b564573ca6802292a
Julien Az <julien.azbrg@gmail.com>
2021-01-14 17:40:36 +0100

    finished

--- js/app.js
+++ js/app.js
@@ -1,36 +1,3 @@
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/serviceWorker.js");
 }
-const pwaTrackingListeners = () => {
-  const fireAddToHomeScreenImpression = (event) => {
-    fireTracking("Add to homescreen shown");
-    //will not work for chrome, untill fixed
-    event.userChoice.then((choiceResult) => {
-      fireTracking(`User clicked ${choiceResult}`);
-    });
-    //This is to prevent `beforeinstallprompt` event that triggers again on `Add` or `Cancel` click
-    window.removeEventListener(
-      "beforeinstallprompt",
-      fireAddToHomeScreenImpression
-    );
-  };
-  window.addEventListener("beforeinstallprompt", fireAddToHomeScreenImpression);
-
-  //Track web app install by user
-  window.addEventListener("appinstalled", (event) => {
-    fireTracking("PWA app installed by user!!! Hurray");
-  });
-
-  //Track from where your web app has been opened/browsed
-  window.addEventListener("load", () => {
-    let trackText;
-    if (navigator && navigator.standalone) {
-      trackText = "Launched: Installed (iOS)";
-    } else if (matchMedia("(display-mode: standalone)").matches) {
-      trackText = "Launched: Installed";
-    } else {
-      trackText = "Launched: Browser Tab";
-    }
-    fireTracking(track);
-  });
-};
--- serviceWorker.js
+++ serviceWorker.js
@@ -10,41 +10,42 @@ self.addEventListener("install", (e) => {
   );
 });
 
-// fetch event
-self.addEventListener("fetch", function (event) {
-  console.log("fetch ta mère fils de pute");
-  // event.respondWith(
-  //   caches.match(event.request).then(function (response) {
-  //     // Cache hit - return response
-  //     if (response) {
-  //       return response;
-  //     }
-  //     // IMPORTANT: Cloner la requête.
-  //     // Une requete est un flux et est à consommation unique
-  //     // Il est donc nécessaire de copier la requete pour pouvoir l'utiliser et la servir
-  //     var fetchRequest = event.request.clone();
-  //     return fetch(fetchRequest).then(function (response) {
-  //       if (!response || response.status !== 200 || response.type !== "basic") {
-  //         return response;
-  //       }
-  //       // IMPORTANT: Même constat qu'au dessus, mais pour la mettre en cache
-  //       var responseToCache = response.clone();
-  //       caches.open(staticCacheName).then(function (cache) {
-  //         cache.put(event.request, responseToCache);
-  //       });
-  //       return response;
-  //     });
-  //   })
-  // );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      // IMPORTANT: Cloner la requête.
+      // Une requete est un flux et est à consommation unique
+      // Il est donc nécessaire de copier la requete pour pouvoir l'utiliser et la servir
+      var fetchRequest = event.request.clone();
+
+      return fetch(fetchRequest).then(function (response) {
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
+
+        // IMPORTANT: Même constat qu'au dessus, mais pour la mettre en cache
+        var responseToCache = response.clone();
+
+        caches.open(staticCacheName).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
+  );
 });
 
 // supprimer caches
-self.addEventListener("activate", (evt) => {
-  //console.log('service worker activated');
-  evt.waitUntil(
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
     caches.keys().then((keys) => {
-      //console.log(keys);
-      return Promise.all(
+      return Promise.add(
         keys
           .filter((key) => key !== staticCacheName)
           .map((key) => caches.delete(key))
